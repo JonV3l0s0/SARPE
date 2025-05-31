0cb7e75532159342e81e753167fddc65f374436a
JonV3l0s0 <jonathanvsantos2010@hotmail.com>
2025-05-31 17:59:32 -0300

    Tela de clientes e funcao de criar clientes
    Criei a tela de criacao de clientes, ainda não tem a funcao de editar o que vai ser cadastrado e etc

---
+++ SARPE/DTO/ClienteCriarDTO.cs
@@ -0,0 +1,17 @@
+﻿using SARPE.Enums;
+using SARPE.Models;
+
+namespace SARPE.DTO
+{
+    public class ClienteCriarDTO
+    {
+        public ETipoEntidade? TipoEntidade { get; set; } = ETipoEntidade.Cliente;
+        public string CnpjCpf { get; set; } = string.Empty;
+        public string RazaoSocial { get; set; } = string.Empty;
+        public string Endereco { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public string Telefone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool IsAtivo { get; set; } = true;
+    }
+}
