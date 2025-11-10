f29fb93dd1032eeaf33f2c8c9706d16b4a039ef3
fabianoalarcon <fabiano.alarcon@gmail.com>
2025-11-10 20:54:19 -0300

    Create IProdutoRepository.cs

---
+++ SARPE/Contracts/IProdutoRepository.cs
@@ -0,0 +1,13 @@
+﻿using SARPE.Models;
+
+namespace SARPE.Contracts
+{
+    public interface IProdutoRepository
+    {
+        IEnumerable<Produto> GetTodosOsProdutos();
+        Produto? GetProdutoPorId(int id);
+        void SalvarProduto(Produto produto);
+        void AtualizarProduto(Produto produto);
+        void ExcluirProdutoPorId(int id);
+    }
+}
