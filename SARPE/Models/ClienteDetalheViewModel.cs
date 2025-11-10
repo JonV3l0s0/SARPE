19a814bced3a6cd6d42236e70567a42360841231
Jonathan <110506767+JonV3l0s0@users.noreply.github.com>
2025-11-10 20:59:27 -0300

    Adicao dos arquivos de models

---
+++ SARPE/Models/ClienteDetalheViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SARPE.Models
+{
+    public class ClienteDetalheViewModel
+    {
+        public int Id { get; set; }
+        public string CnpjCpf { get; set; } = string.Empty;
+        public string RazaoSocial { get; set; } = string.Empty;
+        public string Endereco { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public string Telefone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool IsAtivo { get; set; } = true;
+    }
+}
---
+++ SARPE/Models/Produto.cs
@@ -0,0 +1,66 @@
+﻿
+
+using SARPE.DTO;
+using System.Diagnostics;
+
+namespace SARPE.Models
+{
+    public class Produto : IEquatable<Produto?>
+    {
+        public int Id { get;  set; }
+        public Estoque? EstoqueId { get; set; } 
+        public string CodigoDeBarras { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public float Preco { get; set; }
+        public string Lote { get; set; } = string.Empty;
+
+        #region Construtor
+        public Produto(string codigoDeBarras, string nome, string descricao, float preco, string lote)
+        {
+            CodigoDeBarras = codigoDeBarras;
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+            Lote = lote;
+        }
+
+        // Construtor com Id para fins de teste
+        public Produto(int id, string codigoDeBarras, string nome, string descricao, float preco, string lote)
+        {
+            Id = id;
+            CodigoDeBarras = codigoDeBarras;
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+            Lote = lote;
+        }
+
+        // Provavelmente precisa-se de construtor sem argumentos por conta do EntityFramework
+        // obs: Averiguar essa informação
+        public Produto()
+        {
+        }
+        #endregion
+
+        #region HashCode e Equals
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Produto);
+        }
+
+        public bool Equals(Produto? other)
+        {
+            return other is not null &&
+                   Id == other.Id &&
+                   CodigoDeBarras == other.CodigoDeBarras;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, CodigoDeBarras);
+        }
+        #endregion
+
+    }
+}
