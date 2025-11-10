d09d2f32950966c5ac86d4ac9cb7c5c9a1f54755
fabianoalarcon <fabiano.alarcon@gmail.com>
2025-11-10 20:54:39 -0300

    Create IProdutoService.cs

---
+++ SARPE/Contracts/IProdutoService.cs
@@ -0,0 +1,14 @@
+using SARPE.DTO;
+using SARPE.Models;
+
+namespace SARPE.Contracts
+{
+    public interface IProdutoService
+    {
+        IEnumerable<Produto> GetTodosOsProdutos();
+        Produto? GetProdutoPorId(int id);
+        void SalvarProduto(ProdutoCriarDTO produtoDTO);
+        void AtualizarProduto(int id, ProdutoEditarDTO produtoDTO);
+        void ExcluirProdutoPorId(int id);
+    }
+}
